73ceda7d70d4bfabaaa8a70376616ea81ba86eba
Angus-Harper <aharper1@myjmc.edu.au>
2022-03-07 15:57:14 +1100

    Week 4 proto 2

---
+++ Assets/Prototype 2/Scripts/PlayerControler3D  MonoBehaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler3D : MonoBehaviour
+{
+    public CharacterController controller;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Universal/Scripts/PauseControler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseControler : GameBehaviour
+{
+
+    public GameObject pausePanel;
+    bool pause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pause = false;
+        pausePanel.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        Debug.Log("fdesge");
+        pause = !pause;
+        pausePanel.SetActive(pause);
+        Time.timeScale = pause ? 0 : 1; // changes game speed 0 = stop, 0.5 = half, 1 = full speed, 2 = double speed
+    }
+}
