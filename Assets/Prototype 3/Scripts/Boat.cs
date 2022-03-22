7c3145886d09e64e5e62b1616b83b9f976c49e02
Angus-Harper <86222696+Angus-Harper@users.noreply.github.com>
2022-03-23 00:50:48 +1100

    week 6 part 2

--- Assets/Prototype 3/Scripts/Boat.cs
+++ Assets/Prototype 3/Scripts/Boat.cs
@@ -5,45 +5,45 @@ using UnityEngine;
 public class Boat : MonoBehaviour
 {
     public float speed = 0.2f;
+    public float force = 0.05f;
+    float rotate;
     public int rot;
-    float x = 5;
+    public float timer;
+    float maxTime = 500f / 60;
     // Start is called before the first frame update
     void Start()
     {
+        timer = maxTime;
         randomRotate();
     }
 
     // Update is called once per frame
     void Update()
     {
+        timer = timer - 1f * Time.deltaTime;
         float x = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.forward * x * speed);
 
+        if (timer <= 0)
+        {
+            randomRotate();
+        }
         if (rot == 0)
         {
-            StartCoroutine(Wait());
         }
         else if (rot == 1)
         {
-            transform.Rotate(Vector3.forward * speed);
-            StartCoroutine(Wait());
+            transform.Rotate(Vector3.forward * force);
         }
         else if (rot == 2)
         {
-            transform.Rotate(Vector3.forward * -speed);
-            StartCoroutine(Wait());
+            transform.Rotate(Vector3.forward * -force);
         }
     }
 
     private void randomRotate()
     {
         rot = Random.Range(0, 3);
-    }
-
-    IEnumerator Wait()
-    {
-        Debug.Log("Hello");
-        yield return new WaitForSeconds(x);
-        randomRotate();
+        timer = maxTime;
     }
 }
